8814b5d7bcc359e6f2c4ba24eca23d8c6372f094
Dante05S <Dante05S@hotmail.com>
2023-08-03 15:15:23 -0500

    :bug: fix build

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,3 +1,6 @@
+import { NextResponse } from "next/server";
 
 
-export async function GET(request: Request) {}
+export async function GET(request: Request) {
+    return NextResponse.json({ message: 'Welcome to rotulos-etirrollos' })
+}
